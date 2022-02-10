e96dbaa31748f0a976f73b81a0e45625b8f625ab
Danil Deminov <demiroflmad@gmail.com>
2022-02-10 22:02:31 +0300

    Создана модель записи

---
+++ lib/models/RecordTable.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Demirofl\Calendar\Models;
+
+use Bitrix\Main\Entity\DataManager;
+use Bitrix\Main\ORM\Fields\DatetimeField;
+use Bitrix\Main\ORM\Fields\EnumField;
+use Bitrix\Main\ORM\Fields\IntegerField;
+use Bitrix\Main\ORM\Fields\TextField;
+
+use Bitrix\Main\ORM\Fields\Relations\Reference;
+use Bitrix\Main\ORM\Query\Join;
+
+class RecordTable extends DataManager
+{
+    public static function getTableName()
+    {
+        return "demirofl_calendar_record";
+    }
+
+    public static function getMap()
+    {
+        return [
+            new IntegerField(
+                "ID",
+                [
+                    "primary" => true,
+                    "autocomplete" => true,
+                    "title" => "ID"
+                ]
+            ),
+            new DatetimeField(
+                "DATE",
+                [
+                    "required" => true,
+                ]
+            ),
+            new TextField(
+                "DESCRIPTION",
+            ),
+            new EnumField(
+                "STATUS",
+                [
+                    "values" => [
+                        "пойду",
+                        "под вопросом",
+                        "не пойду"
+                    ],
+                    "required" => true
+                ]
+            ),
+            new IntegerField(
+                "USER_ID"
+            ),
+
+            (new Reference("USER", \Bitrix\Main\UserTable::class, Join::on("this.USER_ID", "ref.ID")))
+        ];
+    }
+}

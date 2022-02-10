e570b02404169e59595b3f829bc4dfeb99c5283b
Danil Deminov <demiroflmad@gmail.com>
2022-02-10 22:03:28 +0300

    Добавлен загрузчик классов

--- include.php
+++ include.php
@@ -1,1 +1,9 @@
 <?php
+try {
+    \Bitrix\Main\Loader::registerAutoLoadClasses(
+        "demirofl.calendar",
+        [
+            "Demirofl\Calendar\Models\RecordTable" => "lib/models/RecordTable.php",
+        ]
+    );
+} catch (\Bitrix\Main\LoaderException $e) {}

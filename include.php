e1c474f67409ebd659435e41b332bc13480e1932
Danil Deminov <demiroflmad@gmail.com>
2022-02-11 12:29:37 +0300

    Добавлено подключение general класса

--- include.php
+++ include.php
@@ -3,6 +3,7 @@ try {
     \Bitrix\Main\Loader::registerAutoLoadClasses(
         "demirofl.calendar",
         [
+            "Demirofl\Calendar\General\CalendarGeneral" => "lib/classes/general/CalendarGeneral.php",
             "Demirofl\Calendar\Models\RecordTable" => "lib/models/RecordTable.php",
         ]
     );

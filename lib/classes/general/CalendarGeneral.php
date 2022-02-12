65c2d079e00b26afae57ffedbb825e8d8f48386b
Danil Deminov <demiroflmad@gmail.com>
2022-02-12 19:55:55 +0300

    Добавлены src библиотеки jqModal Добавлена их инициализация методом init

--- lib/classes/general/CalendarGeneral.php
+++ lib/classes/general/CalendarGeneral.php
@@ -5,7 +5,11 @@ namespace Demirofl\Calendar\General;
 class CalendarGeneral
 {
     public static function init() {
+        \CJSCore::init(["jquery3"]);
         \Bitrix\Main\Page\Asset::getInstance()->addString("<link rel='stylesheet' href='/bitrix/css/demirofl.calendar/fullcalendar.min.css'>");
+        \Bitrix\Main\Page\Asset::getInstance()->addString("<link rel='stylesheet' href='/bitrix/css/demirofl.calendar/jqModal.css'>");
+
         \Bitrix\Main\Page\Asset::getInstance()->addString("<script src='/bitrix/js/demirofl.calendar/fullcalendar.min.js'></script>");
+        \Bitrix\Main\Page\Asset::getInstance()->addString("<script src='/bitrix/js/demirofl.calendar/jqModal.js'></script>");
     }
 }
